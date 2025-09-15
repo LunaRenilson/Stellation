338cd3cf1e5b4794dadffcb28edb2b7e86f4ce4b
lunarenilson <renilson.c.luna@gmail.com>
2025-09-15 12:35:33 -0300

    database configured'

--- backend/index.ts
+++ backend/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.json());
+// app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
